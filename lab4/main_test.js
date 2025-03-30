545a41904f10bd684d47ef037b5d7496900d52bb
banana1715 <82526396+banana1715@users.noreply.github.com>
2025-03-30 17:14:38 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,35 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  // Launch the browser (set headless: false if you want to see the actions)
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    // Navigate the page to a URL
-    await page.goto('https://pptr.dev/');
+  // Navigate to the Puppeteer documentation site
+  await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+  // Click the search button to reveal the search input
+ 
+  await page.waitForSelector('#__docusaurus > nav > div.navbar__inner > div.navbar__items.navbar__items--right > div.navbarSearchContainer_IP3a > button');
+await page.click('#__docusaurus > nav > div.navbar__inner > div.navbar__items.navbar__items--right > div.navbarSearchContainer_IP3a > button');
 
-    // Close the browser
-    await browser.close();
+
+
+  // Type "andy popoo" into the search box with a slight delay between keystrokes
+  await page.waitForSelector('#docsearch-input');
+  await page.type('#docsearch-input', 'andy popoo');
+  
+
+  // Wait for search results to appear
+  await page.waitForSelector('#docsearch-hits1-item-4 > a');
+  await page.click('#docsearch-hits1-item-4 > a');
+
+
+  
+  const title = await page.$eval(
+    '#__docusaurus_skipToContent_fallback > div > div > main > div > div > div.col.docItemCol_nDJs > div > article > div.theme-doc-markdown.markdown > header > h1',
+    el => el.textContent.trim()
+);
+console.log(title);
+await browser.close();
 })();
