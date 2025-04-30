672b5c1cb5761c2eb38de5336bbabbe66101f330
banana1715 <82526396+banana1715@users.noreply.github.com>
2025-05-01 01:37:35 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,10 @@
+
 #include <string.h>
 
-void antiasan(unsigned long addr)
-{
-
+extern char gS[]; 
+extern char gBadBuf[]; 
+extern void __asan_unpoison_memory_region(void const volatile *addr, size_t size);
+void antiasan(unsigned long addr) {
+    __asan_unpoison_memory_region(gS, 0xa7);
+    __asan_unpoison_memory_region(gBadBuf, 0xa7);
 }
